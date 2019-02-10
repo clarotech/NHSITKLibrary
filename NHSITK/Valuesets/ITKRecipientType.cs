7e54e4557859c4b782ff7cb6146bba96c2d53518
clarotech <clarotechuk@gmail.com>
2019-02-10 12:18:08 +0000

    Code tidy

--- NHSITK/Valuesets/ITKRecipientType.cs
+++ NHSITK/Valuesets/ITKRecipientType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ClaroTech.NHSITK.Utility;
+﻿using ClaroTech.NHSITK.Utility;
 
 namespace ClaroTech.NHSITK
 {
